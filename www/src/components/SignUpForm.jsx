ce90cdffef707fbd9f39837137a84bb60b9afff0
Philipp Burckhardt <pburckhardt@outlook.com>
2023-04-26 22:17:22 -0400

    Add subscribe logic

--- www/src/components/SignUpForm.jsx
+++ www/src/components/SignUpForm.jsx
@@ -1,12 +1,36 @@
-import { useId } from 'react'
-
+import { useId, useRef } from 'react'
 import { Button } from '@/components/Button'
 
 export function SignUpForm() {
-  let id = useId()
-
+  const id = useId();
+  const inputEl = useRef(null);
+  const subscribe = async (e) => {
+    e.preventDefault();
+ 
+    // 3. Send a request to our API with the user's email address.
+    const res = await fetch('/api/subscribe', {
+      body: JSON.stringify({
+        email: inputEl.current.value,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+    });
+ 
+    const { error } = await res.json();
+ 
+    if (error) {
+      alert(error);
+      return;
+    }
+ 
+    // 5. Clear the input value and show a success message.
+    inputEl.current.value = '';
+    alert( 'Success! 🎉 You are now subscribed to the newsletter.' );
+  };
   return (
-    <form className="relative isolate mt-8 flex items-center pr-1">
+    <form className="relative isolate mt-8 flex items-center pr-1" onSubmit={subscribe} >
       <label htmlFor={id} className="sr-only">
         Email address
       </label>
@@ -15,11 +39,12 @@ export function SignUpForm() {
         type="email"
         autoComplete="email"
         name="email"
+        ref={inputEl}
         id={id}
         placeholder="Email address"
         className="peer w-0 flex-auto bg-transparent px-4 py-2.5 text-base text-white placeholder:text-gray-500 focus:outline-none sm:text-[0.8125rem]/6"
       />
-      <Button type="submit" arrow>
+      <Button type="submit" arrow >
         Get updates
       </Button>
       <div className="absolute inset-0 -z-10 rounded-lg transition peer-focus:ring-4 peer-focus:ring-sky-300/15" />
