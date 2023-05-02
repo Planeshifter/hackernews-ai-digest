e039c33719ed0d367c97f70ab7a2f4aed5686a95
Philipp Burckhardt <pburckhardt@outlook.com>
2023-05-02 10:24:10 -0400

    Update script

--- scripts/create_daily_digest.js
+++ scripts/create_daily_digest.js
@@ -29,58 +29,63 @@ async function main() {
 			continue;
 		}
 		console.log( 'Processing story ' + i + ': ' + story.url );
-		const response = await axios.get( story.url );
-		const html = response.data;
-		const $ = cheerio.load(html);
-		
-		// Remove elements from the HTML:
-		$('script').remove();
-		$('link').remove();
-		$('style').remove();
-		$('noscript').remove();
-		$('meta').remove();
-		$('iframe').remove();
-		$('img').remove();
-		$('nav').remove();
-		$('footer').remove();
-		$('header').remove();
-
-		// Extract the inner text of the entire HTML document:
-		let innerText = $('body').text();
-		
-		// Remove all extra whitespace:
-		innerText = innerText.replace(/\s+/g, ' ');
-		
-		const content = innerText.trim().substring(0, 8000);
 		try {
-			let completion = await openai.createChatCompletion({
-				model: 'gpt-3.5-turbo',
-				messages: [
-					{ role: 'system', content: 'This AI will write a daily digest of the top stories on Hacker News; it will summarize the following submission in an engaging way.' },
-					{ role: 'user', content: content },
-				]
-			}, {
-				timeout: 10000
-			});
-			const summary = completion.data.choices[0].message.content;
-			digest += '### ' + story.title + '\n\n';
-			digest += '#### [Submission URL]('+story.url+') | ' + story.score + ' points | by [' + story.by + '](https://news.ycombinator.com/user?id='+story.by+') | [' + story.descendants + ' comments](https://news.ycombinator.com/item?id='+story.id+')\n\n';
-			digest += summary + '\n\n';
-			
-			const comments = jsonSerializeCompressed( story.comments ).substring(0, 8000);
-			completion =await openai.createChatCompletion({
-				model: 'gpt-3.5-turbo',
-				messages: [
-					{ role: 'system', content: 'This AI will write a daily digest of the top stories on Hacker News; it will summarize the following discussion about the submission in the comments on Hacker News.' },
-					{ role: 'user', content: 'Summary of Submission: '+summary+'.' },
-					{ role: 'user', content: 'Please summarize the following discussion: '+comments },
-				]
-			}, {
-				timeout: 10000
-			});
-			digest += completion.data.choices[0].message.content;
-			digest += '\n\n';
-			await sleep(1000);
+			const response = await axios.get( story.url );
+			const html = response.data;
+			const $ = cheerio.load(html);
+
+			// Remove elements from the HTML:
+			$('script').remove();
+			$('link').remove();
+			$('style').remove();
+			$('noscript').remove();
+			$('meta').remove();
+			$('iframe').remove();
+			$('img').remove();
+			$('nav').remove();
+			$('footer').remove();
+			$('header').remove();
+
+			// Extract the inner text of the entire HTML document:
+			let innerText = $('body').text();
+
+			// Remove all extra whitespace:
+			innerText = innerText.replace(/\s+/g, ' ');
+
+			const content = innerText.trim().substring(0, 6000);
+			try {
+				let completion = await openai.createChatCompletion({
+					model: 'gpt-3.5-turbo',
+					messages: [
+						{ role: 'system', content: 'This AI will write a daily digest of the top stories on Hacker News; it will summarize the following submission in an engaging way.' },
+						{ role: 'user', content: content },
+					]
+				}, {
+					timeout: 10000
+				});
+				const summary = completion.data.choices[0].message.content;
+				digest += '### ' + story.title + '\n\n';
+				digest += '#### [Submission URL]('+story.url+') | ' + story.score + ' points | by [' + story.by + '](https://news.ycombinator.com/user?id='+story.by+') | [' + story.descendants + ' comments](https://news.ycombinator.com/item?id='+story.id+')\n\n';
+				digest += summary + '\n\n';
+
+				const comments = jsonSerializeCompressed( story.comments ).substring(0, 6000);
+				completion =await openai.createChatCompletion({
+					model: 'gpt-3.5-turbo',
+					messages: [
+						{ role: 'system', content: 'This AI will write a daily digest of the top stories on Hacker News; it will summarize the following discussion about the submission in the comments on Hacker News.' },
+						{ role: 'user', content: 'Summary of Submission: '+summary+'.' },
+						{ role: 'user', content: 'Please summarize the following discussion: '+comments },
+					]
+				}, {
+					timeout: 10000
+				});
+				digest += completion.data.choices[0].message.content;
+				digest += '\n\n';
+				await sleep(1000);
+			}
+			catch (error) {
+				console.error('Error generating summary:', error);
+			}
 		}
 		catch (error) {
 			console.error('Error generating daily digest:', error);
