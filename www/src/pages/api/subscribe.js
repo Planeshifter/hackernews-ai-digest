40ebb3a1a63348fd4fbaad30d613ad18aca5f3f1
Philipp Burckhardt <pburckhardt@outlook.com>
2023-06-24 13:54:39 -0400

    Fix lint error

--- www/src/pages/api/subscribe.js
+++ www/src/pages/api/subscribe.js
@@ -1,12 +1,12 @@
 import mailchimp from '@mailchimp/mailchimp_marketing';
- 
+
 mailchimp.setConfig({
 	apiKey: process.env.MAILCHIMP_API_KEY,
 	server: process.env.MAILCHIMP_API_SERVER, // e.g. us1
 });
- 
-export default async (req, res) => {
-	const { email } = req.body; 
+
+const subscribe = async (req, res) => {
+	const { email } = req.body;
 	if (!email) {
 		return res.status(400).json({ error: 'Email is required' });
 	}
@@ -21,3 +21,5 @@ export default async (req, res) => {
 		return res.status(500).json({ error: error.message || error.toString() });
 	}
 };
+
+export default subscribe;
