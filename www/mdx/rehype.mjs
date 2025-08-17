efbec3f79b4db4242b5d7c72fc8ca90a47ab3f24
Philipp Burckhardt <pburckhardt@outlook.com>
2025-08-17 15:02:55 -0500

    Add check

--- www/mdx/rehype.mjs
+++ www/mdx/rehype.mjs
@@ -52,8 +52,10 @@ export const rehypePlugins = [
       transform: (article) => {
         article.children.splice(0, 1)
         let heading = article.children.find((n) => n.tagName === 'h2')
-        article.properties = { ...heading.properties, title: toString(heading) }
-        heading.properties = {}
+        if (heading) {
+          article.properties = { ...heading.properties, title: toString(heading) }
+          heading.properties = {}
+        }
         return article
       },
     },
